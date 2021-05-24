b30489e8d6a6e9d75fbadf1a904adb5568c5be76
Alina517 <51297046+Alina517@users.noreply.github.com>
2021-05-24 22:06:48 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace SRS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxMas = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.massiv = new System.Windows.Forms.GroupBox();
+            this.richTextBoxInput = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxHelp = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSolve = new System.Windows.Forms.Button();
+            this.buttonTask1 = new System.Windows.Forms.Button();
+            this.buttonTask2 = new System.Windows.Forms.Button();
+            this.buttonTask3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxK = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxN = new System.Windows.Forms.TextBox();
+            this.textBoxM = new System.Windows.Forms.TextBox();
+            this.buttonNM = new System.Windows.Forms.Button();
+            this.massiv.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBoxMas
+            // 
+            this.richTextBoxMas.Location = new System.Drawing.Point(9, 30);
+            this.richTextBoxMas.Name = "richTextBoxMas";
+            this.richTextBoxMas.Size = new System.Drawing.Size(241, 100);
+            this.richTextBoxMas.TabIndex = 0;
+            this.richTextBoxMas.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Заполнение массива";
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(174, 290);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 4;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Location = new System.Drawing.Point(9, 352);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(241, 40);
+            this.buttonGenerate.TabIndex = 5;
+            this.buttonGenerate.Text = "Сгенерировать массив";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // massiv
+            // 
+            this.massiv.Controls.Add(this.richTextBoxInput);
+            this.massiv.Controls.Add(this.buttonGenerate);
+            this.massiv.Controls.Add(this.buttonOK);
+            this.massiv.Controls.Add(this.label2);
+            this.massiv.Controls.Add(this.richTextBoxMas);
+            this.massiv.Location = new System.Drawing.Point(12, 12);
+            this.massiv.Name = "massiv";
+            this.massiv.Size = new System.Drawing.Size(275, 409);
+            this.massiv.TabIndex = 6;
+            this.massiv.TabStop = false;
+            this.massiv.Text = "Массив";
+            // 
+            // richTextBoxInput
+            // 
+            this.richTextBoxInput.Location = new System.Drawing.Point(12, 163);
+            this.richTextBoxInput.Name = "richTextBoxInput";
+            this.richTextBoxInput.Size = new System.Drawing.Size(238, 121);
+            this.richTextBoxInput.TabIndex = 6;
+            this.richTextBoxInput.Text = "";
+            // 
+            // richTextBoxHelp
+            // 
+            this.richTextBoxHelp.BackColor = System.Drawing.SystemColors.Menu;
+            this.richTextBoxHelp.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBoxHelp.Location = new System.Drawing.Point(293, 42);
+            this.richTextBoxHelp.Name = "richTextBoxHelp";
+            this.richTextBoxHelp.Size = new System.Drawing.Size(156, 100);
+            this.richTextBoxHelp.TabIndex = 3;
+            this.richTextBoxHelp.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(477, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Выбор задания";
+            // 
+            // buttonSolve
+            // 
+            this.buttonSolve.Location = new System.Drawing.Point(488, 415);
+            this.buttonSolve.Name = "buttonSolve";
+            this.buttonSolve.Size = new System.Drawing.Size(75, 23);
+            this.buttonSolve.TabIndex = 9;
+            this.buttonSolve.Text = "Решить";
+            this.buttonSolve.UseVisualStyleBackColor = true;
+            this.buttonSolve.Click += new System.EventHandler(this.buttonSolve_Click);
+            // 
+            // buttonTask1
+            // 
+            this.buttonTask1.Location = new System.Drawing.Point(488, 28);
+            this.buttonTask1.Name = "buttonTask1";
+            this.buttonTask1.Size = new System.Drawing.Size(75, 23);
+            this.buttonTask1.TabIndex = 10;
+            this.buttonTask1.Text = "Задание 1";
+            this.buttonTask1.UseVisualStyleBackColor = true;
+            this.buttonTask1.Click += new System.EventHandler(this.buttonTask1_Click);
+            // 
+            // buttonTask2
+            // 
+            this.buttonTask2.Location = new System.Drawing.Point(488, 57);
+            this.buttonTask2.Name = "buttonTask2";
+            this.buttonTask2.Size = new System.Drawing.Size(75, 23);
+            this.buttonTask2.TabIndex = 11;
+            this.buttonTask2.Text = "Задание 2";
+            this.buttonTask2.UseVisualStyleBackColor = true;
+            this.buttonTask2.Click += new System.EventHandler(this.buttonTask2_Click);
+            // 
+            // buttonTask3
+            // 
+            this.buttonTask3.Location = new System.Drawing.Point(488, 86);
+            this.buttonTask3.Name = "buttonTask3";
+            this.buttonTask3.Size = new System.Drawing.Size(75, 23);
+            this.buttonTask3.TabIndex = 12;
+            this.buttonTask3.Text = "Задание 3";
+            this.buttonTask3.UseVisualStyleBackColor = true;
+            this.buttonTask3.Click += new System.EventHandler(this.buttonTask3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(485, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Номер столбца";
+            // 
+            // textBoxK
+            // 
+            this.textBoxK.Location = new System.Drawing.Point(488, 175);
+            this.textBoxK.Name = "textBoxK";
+            this.textBoxK.Size = new System.Drawing.Size(74, 20);
+            this.textBoxK.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(485, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Размер массива";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(485, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "N";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(485, 263);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "M";
+            // 
+            // textBoxN
+            // 
+            this.textBoxN.Location = new System.Drawing.Point(518, 233);
+            this.textBoxN.Name = "textBoxN";
+            this.textBoxN.Size = new System.Drawing.Size(44, 20);
+            this.textBoxN.TabIndex = 18;
+            // 
+            // textBoxM
+            // 
+            this.textBoxM.Location = new System.Drawing.Point(518, 256);
+            this.textBoxM.Name = "textBoxM";
+            this.textBoxM.Size = new System.Drawing.Size(43, 20);
+            this.textBoxM.TabIndex = 19;
+            // 
+            // buttonNM
+            // 
+            this.buttonNM.Location = new System.Drawing.Point(518, 282);
+            this.buttonNM.Name = "buttonNM";
+            this.buttonNM.Size = new System.Drawing.Size(45, 23);
+            this.buttonNM.TabIndex = 20;
+            this.buttonNM.Text = "OK";
+            this.buttonNM.UseVisualStyleBackColor = true;
+            this.buttonNM.Click += new System.EventHandler(this.buttonNM_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 450);
+            this.Controls.Add(this.buttonNM);
+            this.Controls.Add(this.textBoxM);
+            this.Controls.Add(this.textBoxN);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxK);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonTask3);
+            this.Controls.Add(this.buttonTask2);
+            this.Controls.Add(this.buttonTask1);
+            this.Controls.Add(this.buttonSolve);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBoxHelp);
+            this.Controls.Add(this.massiv);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.massiv.ResumeLayout(false);
+            this.massiv.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxMas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.GroupBox massiv;
+        private System.Windows.Forms.RichTextBox richTextBoxHelp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonSolve;
+        private System.Windows.Forms.RichTextBox richTextBoxInput;
+        private System.Windows.Forms.Button buttonTask1;
+        private System.Windows.Forms.Button buttonTask2;
+        private System.Windows.Forms.Button buttonTask3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxK;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxN;
+        private System.Windows.Forms.TextBox textBoxM;
+        private System.Windows.Forms.Button buttonNM;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SRS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int N = 4, M = 10; //размер массива
+        int[] numbers = new int[4]; //массив для задания 1
+        int[,] fdnumbers = new int[4, 10];//для задания 2
+        int[,] matrix = new int[4, 10]; //для задания 3
+        int Task;// номер задания
+
+        /* Задание 1 */
+        private int ClosestToAverage()
+        {
+            int sum = 0, sr, k = 0, mindist = 100;
+            for(int i = 0; i < N; i++)
+            {
+                sum += numbers[i];//cумма элементов массива
+            }
+            sr = sum / N; // среднее значение
+            for (int i = 0; i < N; i++)
+            {
+                if(mindist > Math.Abs(numbers[i] - sr))//находим ближайшее к среднему
+                {
+                    mindist = Math.Abs(numbers[i] - sr);
+                    k = i;
+                }
+            }
+            return numbers[k];
+        }
+
+        /* Задание 2 */
+        private int MinFreedigitNumber()
+        {
+            int min = 1000;
+            bool check = false;
+            for(int i = 0; i < N; i++)
+            {
+                for(int j = 0; j < M; j++)
+                {
+                    //проверка: число 3-х значное и меньше предыдущего минимального
+                    if((fdnumbers[i, j] > 99) & (fdnumbers[i, j] < min))
+                    {
+                        check = true;
+                        min = fdnumbers[i, j];
+                    }
+                }
+            }
+            if (check)// проверка есть ли 3-х значное число
+                return min;
+            else
+                return -1;
+        }
+
+        /* Задание 3 */
+        private int[,] Matrix(int k)
+        {
+            int[,] modmatrix = new int[N, M - 1];//новый массив для измененной матрицы
+
+            for(int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M - 1; j++)
+                {
+                    if (j < k - 1)//пока не дошли до нужного столбц, ничего не меняем
+                        modmatrix[i, j] = matrix[i, j];
+                    else
+                        modmatrix[i, j] = matrix[i, j + 1]; // пропускаем столбец, кот. нужно вырезать и ставим на его место сдедующий
+                }                                
+            }
+            return modmatrix;
+        }
+
+        /* дальше вспомогательные методы и обработчики нажатий на кнопки*/
+        void GenerateRand() //метод для заполнения массивов случайными числами
+        {
+            Random rand = new Random();
+            if (Task == 1)//для задания 1
+            {
+                //генерируем массив
+                for (int i = 0; i < N; i++)
+                {
+                    numbers[i] = rand.Next(-100, 100);
+                }
+                //выводим в textBox
+                richTextBoxMas.Text = "";
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    richTextBoxMas.Text += numbers[i].ToString() + "; ";
+                }
+            }
+            else if (Task == 2)//для задания 2
+            {
+
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < M; j++)
+                    {
+                        fdnumbers[i, j] = rand.Next(0, 1000);//генерируем массив
+                    }
+                }
+                //выводим в textBox
+                richTextBoxMas.Text = "";
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < M; j++)
+                    {
+                        richTextBoxMas.Text += fdnumbers[i, j].ToString() + "; ";
+                    }
+                    richTextBoxMas.Text += "\n";
+                }
+            }
+            else if (Task == 3)//для задания 3
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < M; j++)
+                    {
+                        matrix[i, j] = rand.Next(-10, 10);//генерируем массив
+                    }
+                }
+                //выводим в textBox
+                richTextBoxMas.Text = "";
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < M; j++)
+                    {
+                        richTextBoxMas.Text += matrix[i, j].ToString() + "; ";
+                    }
+                    richTextBoxMas.Text += "\n";
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Task = 1;
+        }
+
+        /* Кнопка "Решить" */
+        private void buttonSolve_Click(object sender, EventArgs e)
+        {
+            int answer, k;
+            if(Task == 1)// задание 1
+            {
+                answer = ClosestToAverage();//вызываем созданный раннее метод
+                //выводим ответ в сплывающее окно
+                MessageBox.Show("Ближайший к среднему значению элемент:\n\n" + answer.ToString()); 
+            }
+            else if (Task == 2)// задание 2
+            {
+                answer = MinFreedigitNumber();
+                //проверка есть ли трехначное число
+                if (answer == -1)
+                    MessageBox.Show("Нет трехзначных чисел");
+                else
+                    MessageBox.Show("Минимальное трехзначное число: \n\n" + answer.ToString());
+            }
+            else if (Task == 3)// задание 2
+            {
+                k = Convert.ToInt32(textBoxK.Text);//считываем введенное пользователем k
+                var modm = Matrix(k);//вызываем метод
+
+                //вывод полученной матрицы в textBox
+                richTextBoxMas.Text += "\n";
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < M - 1; j++)
+                    {
+                        richTextBoxMas.Text += modm[i, j].ToString() + "; ";
+                    }
+                    richTextBoxMas.Text += "\n";
+                }
+            }
+        }
+
+        /*Кнопка ОК после ввода массива*/
+        //значения вводятся через пробел в одну строчку
+        //для двумерного массива в несколько строчек
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            string array;
+            array = richTextBoxInput.Text;//считываем введенные данные
+            if (Task == 1)//задание 1
+            {
+                string[] a = array.Split(' ');//разделяем данные на подстроки (ражделитель "пробел")
+                numbers = new int[a.Length]; //новая длина массива
+                N = numbers.Length;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    numbers[i] = Int32.Parse(a[i]);//parse переводит строковое значение в int
+                }
+                //выводим массив
+                richTextBoxMas.Text = "";
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    richTextBoxMas.Text += numbers[i].ToString() + "; ";
+                }
+            }
+            else//для 2 и 3 заданий
+            {
+                string[] a = array.Split('\n');//разделяем по строкам
+                string[] b = { "", "" };
+                for(int i = 0; i < a.Length; i++)
+                {
+                    b = a[i].Split(' ');                   
+                }
+                N = a.Length;
+                M = b.Length;
+                fdnumbers = new int[N, M];//новый размер массива
+                matrix = new int[N, M];
+                for (int i = 0; i < N; i++)
+                {
+                    b = a[i].Split(' ');//разделяем на отдельные числа каждую строку
+                    for (int j = 0; j < M; j++)
+                    {
+                        fdnumbers[i, j] = Int32.Parse(b[j]);//заполняем массив
+                        matrix[i, j] = Int32.Parse(b[j]);
+                    }
+                }
+                //вывод массива
+                if (Task == 2)
+                {
+                    
+                    richTextBoxMas.Text = "";
+                    for (int i = 0; i < N; i++)
+                    {
+                        for (int j = 0; j < M; j++)
+                        {
+                            richTextBoxMas.Text += fdnumbers[i, j].ToString() + "; ";
+                        }
+                        richTextBoxMas.Text += "\n";
+                    }
+                }
+                if (Task == 2)
+                {
+                    richTextBoxMas.Text = "";
+                    for (int i = 0; i < N; i++)
+                    {
+                        for (int j = 0; j < M; j++)
+                        {
+                            richTextBoxMas.Text += matrix[i, j].ToString() + "; ";
+                        }
+                        richTextBoxMas.Text += "\n";
+                    }
+                }
+            }
+                
+
+
+        }
+
+        /*Кнопки выбора заданий*/
+        private void buttonTask1_Click(object sender, EventArgs e)
+        {
+            Task = 1;
+            numbers = new int[N];
+            GenerateRand();
+        }
+
+        private void buttonTask2_Click(object sender, EventArgs e)
+        {
+            Task = 2;
+            fdnumbers = new int[N, M];
+            GenerateRand();
+        }
+
+        private void buttonTask3_Click(object sender, EventArgs e)
+        {
+            Task = 3;
+            matrix = new int[N, M];
+            GenerateRand();
+            richTextBoxHelp.Text = "Введите номер столбца";
+        }
+
+        /*Кнопка "сгенерировать массив"*/
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            GenerateRand();
+        }
+
+        /* Кнопка ОК (установка размера массива)*/
+        private void buttonNM_Click(object sender, EventArgs e)
+        {
+            N = Convert.ToInt32(textBoxN.Text);
+            M = Convert.ToInt32(textBoxM.Text);
+        }
+
+        void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
